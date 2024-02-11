bdb71de46228d1cca4eea5e270066ebd8c3bbed4
Jesse Keller <jkeller1ca@gmail.com>
2024-02-10 23:16:20 -0800

    Adds witmotion class

---
+++ components/witmotion/witmotion.cpp
@@ -0,0 +1,48 @@
+/* Copyright (C) 2020-2022 Oxan van Leeuwen
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include "witmotion.h"
+
+#include "esphome/core/helpers.h"
+#include "esphome/core/log.h"
+#include "esphome/core/util.h"
+
+
+static const char *TAG = "witmotion";
+
+using namespace esphome;
+
+void StreamServerComponent::setup() {
+    ESP_LOGCONFIG(TAG, "Setting up witmotion...");
+    };
+
+}
+
+void StreamServerComponent::loop() {
+    this->read();
+}
+
+void StreamServerComponent::read() {
+    int len;
+    while ((len = this->stream_->available()) > 0) {
+        char buf[128];
+        len = std::min(len, 128);
+        this->stream_->read_array(reinterpret_cast<uint8_t*>(buf), len);
+        
+        for(int i=0;i<len;i++)
+            ESP_LOGCONFIG(TAG, "RX: 0x%02x",buf[len]);
+    }
+}
---
+++ components/witmotion/witmotion.h
@@ -0,0 +1,46 @@
+/* Copyright (C) 2020-2022 Oxan van Leeuwen
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include "esphome/core/component.h"
+#include "esphome/components/uart/uart.h"
+
+#include <memory>
+#include <string>
+#include <vector>
+
+class WitmotionComponent : public esphome::Component {
+public:
+    WitmotionComponent() = default;
+    explicit WitmotionComponent(esphome::uart::UARTComponent *stream) : stream_{stream} {}
+    void set_uart_parent(esphome::uart::UARTComponent *parent) { this->stream_ = parent; }
+
+    void setup() override;
+    void loop() override;
+    void dump_config() override;
+    void on_shutdown() override;
+
+    float get_setup_priority() const override { return esphome::setup_priority::AFTER_WIFI; }
+
+	int get_client_count() { return this->clients_.size(); }
+	
+protected:
+    void read();
+    void write();
+
+    esphome::uart::UARTComponent *stream_{nullptr};
+};
